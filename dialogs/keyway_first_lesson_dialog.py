1ee8c160c877f95596b9f7c5ebb28b756e8fc8d1
Vuacheslav <soloomonslava1991@gmail.com>
2026-01-25 00:18:41 +0300

    .

--- dialogs/keyway_first_lesson_dialog.py
+++ dialogs/keyway_first_lesson_dialog.py
@@ -8,14 +8,17 @@ from aiogram_dialog.widgets.media import DynamicMedia, StaticMedia
 from aiogram_dialog.api.entities import MediaAttachment
 from config.config import BASE_DIR
 
-async def first_vebinar_video_getter(**kwargs):
-    video = MediaAttachment(type=ContentType.VIDEO, path= BASE_DIR / "media" / "video" / "vebinar_keyway_1.mov")
-    return {'video': video}
+# async def first_vebinar_video_getter(**kwargs):
+#     video = MediaAttachment(type=ContentType.VIDEO, path= BASE_DIR / "media" / "video" / "vebinar_keyway_1.mov")
+#     return {'video': video}
 
 vebinar = Window(
     Const(text="Запись первого вебинара по обучению Keyway"),
-    DynamicMedia('video'),
-    getter=first_vebinar_video_getter,
+    StaticMedia(
+        path=BASE_DIR / "media" / "video" / "vebinar_keyway_1.mp4",
+        type=ContentType.VIDEO,
+        media_params={"supports_streaming": True},
+    ),
     state=KeywayFirstLessonDialog.vebinar
     )
 
